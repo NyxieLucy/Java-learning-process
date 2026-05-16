59da3f78f34a7fe756479dfe57e55a9ca64d3654
NyxieLucy <nyxlucianis@gmail.com>
2026-05-16 11:14:29 +0100

    chnaged the folder to variables

--- my first project/src/Main.java
+++
@@ -1,4 +0,0 @@
-void main() {
-    int hergawa = 10;
-    System.out.println("the number of hergawa in this room is: " + hergawa);
-}
---
+++ variables/src/Main.java
@@ -0,0 +1,6 @@
+void main() {
+    int year = 2026;
+    double gpa = 3.9;
+    char grade = 'A';
+    System.out.println("this year is: " + year + "ma current gpa is : " + gpa + " with the main grade is: "+ grade);
+}
